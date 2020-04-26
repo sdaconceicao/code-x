548734c5e0b08ede0dd0d88bcfa8e8d7671c0a0f
Stephen da Conceicao <daconcei472@gmail.com>
2020-04-26 16:50:01 -0400

    Updated dependencies

--- packages/checkbox/src/Checkbox.styles.js
+++ packages/checkbox/src/Checkbox.styles.js
@@ -3,5 +3,8 @@ import {createUseStyles} from 'react-jss'
 export default createUseStyles({
     error: {
         borderColor: ({palette}) => palette.error
+    },
+    label: {
+        display: 'block'
     }
 })
