6af0a7f0038f50b871229dfd4c84d0c5f809af4a
Stephen da Conceicao <daconcei472@gmail.com>
2021-01-22 16:56:52 -0500

    Updating storybook to 6.1

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,10 +4,8 @@ module.exports = {
        '../packages/**/*.stories.(js|mdx)',
     ],
     addons: [
-        '@storybook/addon-knobs',
         '@storybook/addon-actions',
         '@storybook/addon-a11y',
-        '@storybook/addon-storysource',
         '@storybook/addon-docs'
     ]
 }
