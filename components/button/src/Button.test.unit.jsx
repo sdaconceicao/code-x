92be98e07cf309d260b9e7120279b4dfa1e4cb16
Stephen da Conceicao <daconcei472@gmail.com>
2021-11-24 10:56:03 -0500

    Adding role tests for buttons

--- components/button/src/Button.test.unit.jsx
+++ components/button/src/Button.test.unit.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Button from './Button';
 
 describe('Button', () => {
   it('renders', () => {
-    const { getByText } = render(<Button>Lorem</Button>);
-    expect(getByText('Lorem')).toBeTruthy();
+    const { getByRole } = render(<Button>Lorem</Button>);
+    expect(getByRole('button', { name: 'Lorem' })).toBeTruthy();
   });
+  it('returns an onClick event', () =>{
+    const spy = jest.fn();
+    const { getByRole } = render(<Button onClick={spy}>Lorem</Button>);
+    fireEvent.click(getByRole('button', { name: 'Lorem' }))
+    expect(spy).toBeCalledTimes(1);
+  })
 });
