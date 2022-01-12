d7880c5913657ec6754193aa2115eed1de644189
Stephen da Conceicao <daconcei472@gmail.com>
2022-01-11 23:26:29 -0500

    fix: jest ignore collecting coverage in docs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/**/*.{js,jsx}', '!<rootDir>/**/documentation/**/*.{js,jsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/**/*.{js,jsx}',
+    '!<rootDir>/**/documentation/**/*.{js,jsx}',
+    '!<rootDir>/docs/**/*.{js,jsx}'
+  ],
   moduleDirectories: ['node_modules'],
   rootDir: './',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
