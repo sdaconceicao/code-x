0cf1012c8936ace0d5c2370aa9cc1879e4da2a6d
Stephen da Conceicao <daconcei472@gmail.com>
2022-01-17 11:54:15 -0500

    docs: Added custom theming to storybook

---
+++ apps/documentation/.storybook/theme.js
@@ -0,0 +1,34 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Code-X',
+  brandUrl: 'https://sdaconceicao.github.io/code-x',
+
+  colorPrimary: '#ff0000',
+  colorSecondary: '#cc0000',
+
+  // UI
+  appBg: '#555',
+  appContentBg: '#eee',
+  appBorderColor: 'grey',
+  appBorderRadius: 4,
+  textColor: '#000',
+  textInverseColor: '#fff',
+  textMutedColor: '#444',
+
+  // Typography
+  fontBase: '"Open Sans", sans-serif',
+  fontCode: 'monospace',
+
+  // Toolbar default and active colors
+  barTextColor: 'silver',
+  barSelectedColor: '#222',
+  barBg: 'grey',
+
+  // Form colors
+  inputBg: 'grey',
+  inputBorder: 'silver',
+  inputTextColor: 'white',
+  inputBorderRadius: 4
+});
