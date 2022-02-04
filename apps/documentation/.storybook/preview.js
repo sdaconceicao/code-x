83e59b274a985ccb8187fe38ffba45118b68f0b0
Stephen da Conceicao <daconcei472@gmail.com>
2022-02-03 19:52:05 -0500

    docs: Updated background color on theming switch

--- apps/documentation/.storybook/preview.js
+++ apps/documentation/.storybook/preview.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { addParameters, addDecorator } from '@storybook/react';
+import { addParameters } from '@storybook/react';
 import { DocsPage, DocsContainer } from '@storybook/addon-docs';
 import { withThemes } from '@react-theming/storybook-addon';
-import { ThemeProvider, createUseStyles } from 'react-jss';
+import { ThemeProvider } from 'react-jss';
 import { I18nProvider } from '@code-x/i18n';
 import theme, { dark } from '@code-x/theme';
 import { enUSMessages as enUSLabelMessages } from '@code-x/label';
@@ -11,22 +11,20 @@ import { enUSMessages as enUSSelectMessages } from '@code-x/select';
 import storybookTheme from './theme';
 
 const messages = { ...enUSLabelMessages, ...enUSValidationMessages, ...enUSSelectMessages };
-const useStyles = createUseStyles({
-  '@global': {
-    html: {
-      boxSizing: 'border-box'
-    },
-    '*': {
-      boxSizing: 'inherit'
-    }
-  }
-});
 
 const withI18nProvider = (Story) => (
   <I18nProvider messages={messages} locale="en">
     <Story />
   </I18nProvider>
 );
+
+const onThemeSwitch = (context) => ({
+  parameters: {
+    backgrounds: {
+      default: context.theme.name === 'dark' ? '#444' : '#bbb'
+    }
+  }
+});
 
 addParameters({
   docs: {
@@ -36,4 +34,7 @@ addParameters({
   }
 });
 
-export const decorators = [withThemes(ThemeProvider, [theme, dark]), withI18nProvider];
+export const decorators = [
+  withThemes(ThemeProvider, [theme, dark], { onThemeSwitch }),
+  withI18nProvider
+];
